4c5ebfb3b9e6376c0bfee5dae1efa9539ac705f0
Ravi Panchal <pancha.ravi@gmail.com>
2017-07-24 10:09:25 -0400

    checking code in to pngstream branch

--- public/javascripts/ar-drone.js
+++ public/javascripts/ar-drone.js
@@ -3,41 +3,56 @@ var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
 var router = express.Router();
 
-
-/* GET home page. */
+//Function to Takeoff Drone 
 function takeoff() {
   client.takeoff();
+  client.after(2000,function(){
+  	console.log("Now Hovering");
+  	client.stop();
+  });
 };
-
 exports.takeoff =takeoff;
 
+
+//Function to Land Drone 
 function land() {
   client.land();
 };
 exports.land =land;
 
+
+//Function to Stop(Hover) Drone 
 function stop() {
   client.stop();
 };
 exports.stop=stop;
 
+//Function to Rotate Drone 90 Degrees
 function rotate90() {
   client.clockwise(0.5);
+  client.after(function(){
+  	console.log("Now Rotating");
+  	this.stop();
+  });
+  client.stop();
 };
 exports.rotate90=rotate90;
 
+//Function to Rotate Drone 180 Degrees
 function rotate180() {
   client.clockwise(1.0);
+    client.stop();
 };
-
 exports.rotate180=rotate180;
 
+
+//Function to Rotate Drone 270 Degrees
 function rotate270() {
   client.clockwise(1.5);
 };
-
 exports.rotate270=rotate270;
 
+//Function to Rotate Drone 360 Degrees 
 function rotate360() {
   client.clockwise(2.0);
 };
--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,18 @@ router.get('/', function(req, res,next) {
 router.post('/takeoff', function(req, res,next) {
 	console.log('Flying Drogon');
 	Drogon.takeoff();
-	res.render('index');
-
+	console.log('Takeoff Successfull and now hovering');
 });
+
+
+	// SC.get('/tracks', {
+	//  genres: "todays Hits"	  
+	// }).then(function(tracks) {
+	//     var searchresultset = tracks;
+	//     //console.log(searchresultset);
+	//     renderList(searchresultset)
+	// });
+
 
 //Land Function 
 router.post('/land', function(req, res,next) {
@@ -31,13 +40,23 @@ router.post('/stop', function(req, res,next) {
 	res.render('index');
 });
 
-//Rotate 90 Degrees
-router.post('/rotate90', function(req, res,next) {
-	console.log('Rotataing Drogon to 90 Degrees');
-	Drogon.rotate90().then(Drogon.stop())
-	res.render('index');
-});
+//Rotate 90 Degrees working
+// router.post('/rotate90', function(req, res,next) {
+// 	console.log('Rotataing Drogon to 90 Degrees');
+// 	Drogon.rotate90()
+// 	res.render('index');
+// });
 
+//Rotate 90 Degrees with promise
+router.post('/rotate90', function(req, res,next) {
+	console.log('Rotating Drogon to 90 Degrees');
+	var rotate90 = new Promise (function (reject,resolve){
+	console.log(rotate90);	
+	Drogon.rotate90();
+	}).then(function (){
+   console.log('Rotating Successfull');
+	});
+});	
 
 //Rotate 180 Degrees
 router.post('/rotate180', function(req, res,next) {
@@ -52,8 +71,5 @@ router.post('/rotate270', function(req, res,next) {
 	Drogon.rotate270();
 	res.render('index');
 });
-
-
-
 
 module.exports = router;
